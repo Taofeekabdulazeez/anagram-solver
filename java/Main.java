8018eaf246c745fcce0b8efe873f111703437fc1
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-12-06 12:59:27 +0100

    minor development:[add Java implementation]

---
+++ java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+   public static void main(String[] args) {
+     System.out.println("Hello, Anagram");
+     System.out.println(Anagram.checkAnagram("heaters", "aethers"));
+   }
+}
